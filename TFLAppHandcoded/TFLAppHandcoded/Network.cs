4a1f601e7caad058b13e71f210570423b33ecb95
Galina Abdurashitova <g.m.abdurashitova@gmail.com>
2024-05-09 13:47:02 +0100

    Squashed commit of the following:
    commit a5653312b61ec9c3aae23eeac79e7ccc07947e36
    Merge: ae9cc85 d8b19a1
    Author: Galina Abdurashitova <g.m.abdurashitova@gmail.com>
    Date:   Thu May 9 13:43:41 2024 +0100
    
        Merge pull request #4 from TFL-application/Network
    
        Code added for Menu.cs, Program.cs and minor changes in Network.cs
    
    commit d8b19a1f6f6c306ae462f38698c2736f4e8e186b
    Author: Gurpreet Kaur Mahun <priti.kaur888@gmail.com>
    Date:   Sun May 5 10:31:27 2024 +0100
    
        edit on Menu.cs
    
    commit a7ebacf62ddbd825cedbffb01a82d55773b48e09
    Author: Gurpreet Kaur Mahun <priti.kaur888@gmail.com>
    Date:   Sun May 5 10:21:12 2024 +0100
    
        code added for Menu.cs, Program.cs and minor changes in Network.cs
    
    commit ae9cc8560152e9067f4d20d03847bbc094bb4e51
    Author: Gurpreet Kaur Mahun <priti.kaur888@gmail.com>
    Date:   Tue Apr 30 12:35:45 2024 +0100
    
        Update Network.cs
    
        Added code for CLose Track and open track
    
    commit 14b8bb0c2c63f376f56c730e24704ebb6f647df4
    Author: Kash <katarzyna.radek@outlook.com>
    Date:   Mon Apr 29 13:02:26 2024 +0100
    
        adding networkData and stations
    
    commit 99667c818861796aed6bb014ded7cf889b207922
    Author: Gurpreet Kaur Mahun <priti.kaur888@gmail.com>
    Date:   Sun Apr 28 00:46:01 2024 +0100
    
        Network class Code
    
    commit 6d3ac94ce96fe61f4ee4df5365f5daa19ea3a3a1
    Merge: e5be23f 6a4fc18
    Author: Galina Abdurashitova <g.m.abdurashitova@gmail.com>
    Date:   Sat Apr 27 20:49:08 2024 +0100
    
        Merge pull request #2 from TFL-application/Station-class
    
        Station class
    
    commit 6a4fc181b4d736a9dc27fdc1d38596978ecd6018
    Merge: 316957b e5be23f
    Author: Galina Abdurashitova <g.m.abdurashitova@gmail.com>
    Date:   Sat Apr 27 20:48:59 2024 +0100
    
        Merge branch 'main' into Station-class
    
    commit e5be23f86d66a17849396f9f149658fc0805f7f7
    Merge: 3b9c544 58d653d
    Author: Galina Abdurashitova <g.m.abdurashitova@gmail.com>
    Date:   Sat Apr 27 20:36:48 2024 +0100
    
        Merge pull request #1 from TFL-application/Dictionary
    
        Add Dictionary
    
    commit 316957b6b647ee1006ec46ec415d9750a3c85561
    Author: Naima9991 <150269222+Naima9991@users.noreply.github.com>
    Date:   Sat Apr 27 19:38:01 2024 +0100
    
        Station class
    
    commit 3b9c544d7dc65c02df85d57427d8191cbc8e96bf
    Author: Zara Khan <92478647+zarakhan99@users.noreply.github.com>
    Date:   Sat Apr 27 14:48:37 2024 +0100
    
        Line Draft 1

--- TFLAppHandcoded/TFLAppHandcoded/Network.cs
+++ TFLAppHandcoded/TFLAppHandcoded/Network.cs
@@ -2,20 +2,202 @@
 using TFLAppHandcoded.Interfaces;
 using TFLAppHandcoded.Dictionary;
 
-namespace TFLAppHandcoded
-{
-	public class Network
-	{
+namespace TFLAppHandcoded{
+
+    public class NetworkData
+    {
+        public Dictionary<string, Line> Lines { get; private set; }
+    }
+
+    public class Network:INetwork{
+
         private double changeLineTime = 2.0;
 
-		public Network()
-		{
-		}
+        private NetworkData networkData;
+        private Line[]lines;
+        private LinkedList<Track> closedTracks;
+        private LinkedList<Track> delayedTracks;
+
+        public Network(NetworkData networkData)
+        {
+            this.networkData = networkData;
+            InitilizeNetwork(networkData);
+        }
+
+        private void InitilizeNetwork(networkData)
+        {
+            var circleBakerStreet = new Station("Baker Street");
+        }
+
+        public Network(){}
+
+
+        //return type void is added because it might be required for the menu to display line
+        public void GetLines(){
+            int lineLength=lines.length();
+            try{
+                if(lineLength<=0){
+                    throw new ArgumentException("No valid Data to Show");
+                }
+
+                for(int i=0;i<lineLength;i++){
+                    Console.Writeline($"Line : {lines[i].name}");
+                    Console.Writeline($"Line : {lines[i].color}");
+                }
+            }catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+
+
+
+        }
+
+
+        public string GetStation(string linename){
+            try{
+                foreach(Line line in lines){
+                    if(line.GetStation().GetName()==linename){
+                        return line.GetStation();
+                }
+
+            }}
+             throw new ArgumentException($"Station name : {name} doesnot falls on the line");
+            catch(Exception ex){
+                 Console.WriteLine("An error occured:",ex.message);
+            }
+            
+            }
+
+        
+        public IStation GetAllStation(string linename){
+
+            try{
+                foreach (Line line in lines){
+                if(this.line.GetName()==linename){
+                    return line.GetAllStations();
+                }
+                
+            }
+            throw new ArgumentException($"Line name {line.GetName()} not Found");
+            }
+            catch (Exception ex){
+                Console.WriteLine("An error occured:",ex.message);
+            }
+            
+        }
+
+        //I have added Line attirbute:
+        public void AddTimeDelay(ILine l, string stationFrom,string stationTo, double time, bool bothDirections){
+
+            // FindRecordValueWithKey
+            try{
+               foreach(Iline line in lines){
+
+                //.CheckKey method taken from handcoded Dicitonary vand FInditem taken from weighted Linked list
+                //Implementing checks to find if the stationTo exists as a record in Adjlist in Line Class and stationFrom exists as a value in connected station
+                if (line.GetName()==l.GetName()){
+
+                    if(line.GetStation().CheckKeyExists(stationFrom&&line.GetName().FindItem(stationTo))){
+                        line.GetStation[stationFrom].FindRecordValueWithKey(stationTo).SetDelay(time);
+                    }
+                    if (bothDirections){
+                        line.GetStation[stationTo].FindRecordValueWithKey(stationFrom).SetDelay(time);
+                    }
+
+                }
+               }
+
+            }catch(Exception ex){
+                    Console.WriteLine("An error occured:",ex.message);
+            }
+
+
+        }
+
+        //Boothdirection Attribute is added to this method as well as Delay could or could not be in both directions
+        public void DeleteTimeDelay(ILine l, string stationFrom,string stationTo, double time,bool bothDirections){
+              try{
+               foreach(Iline line in lines){
+
+                //.CheckKey method taken from handcoded Dicitonary vand FInditem taken from weighted Linked list
+                //Implementing checks to find if the stationTo exists as a record in Adjlist in Line Class and stationFrom exists as a value in connected station
+                if (line.GetName()==l.GetName()){
+
+                    if(line.GetStation().CheckKeyExists(stationFrom&&line.GetName().FindItem(stationTo))){
+
+                        //for removing time delay The previously set delay is set back to 0.0
+                        line.GetStation[stationFrom].FindRecordValueWithKey(stationTo).SetDelay(0.0);
+                    }
+                    if (bothDirections){
+                        line.GetStation[stationTo].FindRecordValueWithKey(stationFrom).SetDelay(0.0);
+                    }
+
+                }
+               }
+
+            }catch(Exception ex){
+                    Console.WriteLine("An error occured:",ex.message);
+            }
+
+        }
+
+
+        //Removed time and bothdirection attribute 
+        public CloseTrack(string stationFrom,string stationTo){
+
+             try{
+               foreach(Iline line in lines){
+
+                //.CheckKey method taken from handcoded Dicitonary vand FInditem taken from weighted Linked list
+                //Implementing checks to find if the stationTo exists as a record in Adjlist in Line Class and stationFrom exists as a value in connected station
+                if (line.GetName()==l.GetName()){
+
+                    if(line.GetStation().CheckKeyExists(stationFrom&&line.GetName().FindItem(stationTo))){
+                        line.GetStation[stationFrom].FindRecordValueWithKey(stationTo).SetIsOpen(false);
+                        line.GetStation[stationTo].FindRecordValueWithKey(stationFrom).SetIsOpen(false);
+                    }
+                    
+
+                }
+               }
+
+            }catch(Exception ex){
+                    Console.WriteLine("An error occured:",ex.message);
+            }
+        }
+
+
+
+
+        public void OpenTrack(line: ILine, stationFrom: string, stationTo: string){
+              try{
+               foreach(Iline line in lines){
+
+                //.CheckKey method taken from handcoded Dicitonary vand FInditem taken from weighted Linked list
+                //Implementing checks to find if the stationTo exists as a record in Adjlist in Line Class and stationFrom exists as a value in connected station
+                if (line.GetName()==l.GetName()){
+
+                    if(line.GetStation().CheckKeyExists(stationFrom&&line.GetName().FindItem(stationTo))){
+                        line.GetStation[stationFrom].FindRecordValueWithKey(stationTo).SetIsOpen(true);
+                        line.GetStation[stationTo].FindRecordValueWithKey(stationFrom).SetIsOpen(true);
+                    }
+                    
+
+                }
+               }
+
+            }catch(Exception ex){
+                    Console.WriteLine("An error occured:",ex.message);
+            }
+
+        }
+
 
         public WeightedLinkedList<IStation, double>? FindShortestPath(IStation startStation, IStation destinationStation)
 		{
             return FastestPathAlgorithm.GetFastestPath(startStation, destinationStation, changeLineTime);
 		}
+
     }
 }
-
